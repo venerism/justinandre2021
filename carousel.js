6d4b28acc52e7a066c2b0f7a8f5c4468bbbd6e27
Justin veneracion <justin.veneracion11@gmail.com>
2020-12-30 23:34:06 -0500

    adds favicon

---
+++ carousel.js
@@ -0,0 +1,21 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter =1;
+const size =carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'tranlateX(' + (size * counter) + 'px) ';
+
+//Button Listeners
+
+nextBtn.addEventListener('click',()=>{
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  console.log(counter)
+});
